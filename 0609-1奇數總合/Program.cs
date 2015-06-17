50fc0de2a429a6e9b0d0cf40c6a84278e32916f2
bensonchen <bensonat62@gmail.com>
2015-06-17 16:18:16 +0800

    0609 奇數總和, 整數累加

---
+++ 0609-1奇數總合/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example4_odd
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入整數, 計算奇數累加總合:");
+            int i = 1;
+            int j = int.Parse(Console.ReadLine());
+            Console.WriteLine("奇數有 :");
+            int total = 0;
+            for (i = 1; i <= j; i = i + 2)
+            {
+                Console.Write("  {0}", i);
+                total = total + i;
+            }
+            Console.WriteLine();
+            Console.WriteLine("奇數累加為{0}", total);
+        }
+    }
+}
---
+++ 0609-2整數累加/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入整數, 計算累加總合:");
+            int i = 1;
+            int j = int.Parse(Console.ReadLine());
+            int total = 0;
+            for (i = 1; i <= j; i++)
+            {
+                total = total + i;
+                Console.Write("  {0}", total);
+            }
+            Console.WriteLine();
+        }
+    }
+}
